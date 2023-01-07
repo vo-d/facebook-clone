12fe1eec7832a92628e8a2c4ae8abdaeb28d5774
Dai Dai Vo <daidaivo2002@gmail.com>
2023-01-06 22:04:09 -0600

    @layout and @apply for tailwindcss

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ function Header() {
             <Image src="https://upload.wikimedia.org/wikipedia/commons/0/05/Facebook_Logo_%282019%29.png" alt={''} width={40} height={40} ></Image>
             <div className=' flex ml-2 items-center rounded-full bg-gray-100 p-2'>
                 <MagnifyingGlassIcon className=' h-6 text-gray-600'></MagnifyingGlassIcon>
-                <input className=' flex ml-2 items-center bg-transparent outlien-none placeholder-gray-500 flex-shrink' type="text" placeholder='Search Facebook' />
+                <input className='hidden md:inline-flex ml-2 items-center bg-transparent outlien-none placeholder-gray-500 flex-shrink' type="text" placeholder='Search Facebook' />
             </div>
         </div>
         {/*  left  */}
@@ -28,6 +28,13 @@ function Header() {
             </div>
         </div>
         {/* right */} 
+        <div className='flex items-center sm:space-x-2 justify-start'>
+            <p className=' whitespace-nowrap font-semibold pr-3'>Dai Vo</p>
+            <ViewColumnsIcon className='icon'></ViewColumnsIcon>
+            <ChatBubbleOvalLeftIcon className='icon'></ChatBubbleOvalLeftIcon>
+            <BellIcon className='icon'></BellIcon>
+            <ChevronDownIcon className='icon'></ChevronDownIcon>
+        </div>
     </div>
     
   )
