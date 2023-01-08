2adf430fb72e2f6aefde8ebad0458f4783961196
vo-d <daidaivo2002@gmail.com>
2023-01-07 23:30:49 -0600

    next-auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,19 @@
 import NextAuth from "next-auth"
-import providers from "next-auth/providers"
+import facebook from 'next-auth/providers/facebook'
+import google from 'next-auth/providers/google'
 
 export const authOptions = {  
     // Configure one or more authentication providers  
     providers: [    
-        providers.Facebook({      
+        facebook({      
             clientId: process.env.FACEBOOK_CLIENT_ID,      
             clientSecret: process.env.FACEBOOK_CLIENT_SECRET,    
         }),    
         // ...add more providers here  ],}export default NextAuth(authOptions)  
+        google({      
+            clientId: process.env.FACEBOOK_CLIENT_ID,      
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET,    
+        }),
     ],
 }
     
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import Head from 'next/head'
 import Header from '../components/Header'
-import { GetServerSideProps } from 'next/types'
+import  {GetServerSideProps} from 'next'
+import {getSession} from 'next-auth/react'
+import Login from '../components/Login'
 
+interface Props{
+  session : any
+}
 
-export default function Home() {
+export default function Home({session} : Props) {
+  if (!session) return <Login/>
   return (
     <div>
       <Head>
@@ -24,3 +30,12 @@ export default function Home() {
   )
 }
 
+export const getServerSideProps : GetServerSideProps = async(context) =>{
+  // Get the user
+  const session = await getSession(context)
+  return{
+    props:{
+      session
+    }
+  }
+}
