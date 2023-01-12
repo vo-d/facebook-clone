9766aeae4250277d6c421b9303066ff3e50d59e4
vo-d <daidaivo2002@gmail.com>
2023-01-11 20:38:11 -0600

    stories

---
+++ components/Feed.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Feed() {
+  return (
+    <div>
+        <div>
+            {/* Stories */}
+            <Stories />
+            {/* InputBox */}
+            {/* Posts */}
+        </div>
+    </div>
+  )
+}
+
+export default Feed
