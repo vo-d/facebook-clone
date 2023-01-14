af9d11989dcabbee56c15e58d41f54bb9c8603b1
vo-d <daidaivo2002@gmail.com>
2023-01-14 16:15:37 -0600

    input box

--- components/Feed.js
+++ components/Feed.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Stories from './Stories'
+import InputBox from './InputBox'
 
 function Feed() {
   return (
@@ -7,7 +8,7 @@ function Feed() {
         <div className='mx-auto max-w-md md:max-w-lg lg:max-w-2xl'>
             
             <Stories />
-            {/* InputBox */}
+            <InputBox/>
             {/* Posts */}
         </div>
     </div>
---
+++ components/InputBox.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Image from 'next/image';
+import { useSession } from 'next-auth/react';
+import {CameraIcon, VideoCameraIcon } from '@heroicons/react/24/solid'
+import {FaceSmileIcon } from '@heroicons/react/24/outline'
+
+function InputBox() {
+    const {data:session, status} = useSession();
+    const sendPost =(e)=>{
+        e.preventDefault()
+    }
+    return (
+        <div className='bg-white p-2 rounded-2xl shadow-md text-gray-500 font-medium mt-6'>
+            <div className='flex space-x-4 p-4 items-center'>
+                <Image className='rounded-full cursor-pointer' src={session.user.image} width={40} height={40}></Image>
+                <form className='flex flex-1'>
+                    <input className='rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none' type={'text'} placeholder={`What's on your mind, ${session.user.name}?`}></input>
+                    <button hidden type={"submit"} onClick={sendPost}></button>
+                </form>
+            </div>
+            <div className='flex justify-evenly p-3 border-t'>
+                <div className='inputIcon'>
+                    <VideoCameraIcon className='h-7 text-red-500 '></VideoCameraIcon>
+                    <p className='text-xs sm:text-sm xl:text-base'>Live Video</p>
+                </div>
+                <div className='inputIcon'>
+                    <CameraIcon className='h-7 text-green-400 '></CameraIcon>
+                    <p className='text-xs sm:text-sm xl:text-base'>Feeling/Activity</p>
+                </div>
+                <div className='inputIcon'>
+                    <FaceSmileIcon className='h-7 text-yellow-300 '></FaceSmileIcon>
+                    <p className='text-xs sm:text-sm xl:text-base'>Feeling/Activity</p>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default InputBox
